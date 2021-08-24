47e4502d52fec09849d0406029b7d1364dcf96f1
Ram Reddy Amanaganti <ramreddya@biarca.com>
2021-08-24 16:00:17 +0000

    Updating deployment tests
    Signed-off-by: Ram Reddy Amanaganti <ramreddya@biarca.com>

--- features/deployment.feature
+++ features/deployment.feature
@@ -1,7 +1,17 @@
 Feature: Manage deployments
 
+  Scenario: Try retrieve a deployment that does not exist
+    Given A deployment called fog does not exist
+      When The user attempts to retrieve the deployment fog
+        Then returns None
+	
+
   Scenario: Create a deployment and wait for it to become ready
     Given A deployment called whilrwind does not exist
-      When the deployment called whilrwind is created
-      #And the create operation waits for it to become ready
-      Then a valid deployment called whilrwind can be found
+      When The deployment called whilrwind is created
+        Then a valid deployment called whilrwind can be found
+
+  Scenario: Retrieve a deployment that exists
+    Given The deployment called fire exists
+      When The user attempts to retrieve the deployment fire
+        Then Results for the deployment fire are returned
--- features/steps/deployment.py
+++ features/steps/deployment.py
@@ -10,22 +10,47 @@ def step_impl(context, deployment_name):
     deployment.delete(context.k8s_v1_apps_client, deployment_name)
 
 
-@when(u"the deployment called whilrwind is created")
+@when(u"The user attempts to retrieve the deployment {deployment_name}")
+def step_impl(context, deployment_name):
+    context.get_resp = deployment.get(context.k8s_v1_apps_client, deployment_name)
+
+@then(u"returns None")
 def step_impl(context):
+    assert context.get_resp is None, "Did not return None"
+
+@when(u"The deployment called {deployment_name} is created")
+def step_impl(context, deployment_name):
     env = Environment(
         loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
     )
     deployment_tmpl = env.get_template("deployment.j2")
-    deployment_spec_file = deployment_tmpl.render(deployment_name="whilrwind")
-    context.create_whilrwind_resp = deployment.create(
+    deployment_spec_file = deployment_tmpl.render(deployment_name=deployment_name)
+    context.create_resp = deployment.create(
         context.k8s_v1_apps_client, deployment_spec_file
     )
 
 
 @then(u"a valid deployment called whilrwind can be found")
 def step_impl(context):
-    print(context.create_whilrwind_resp.status.replicas)
     assert (
-        context.create_whilrwind_resp.status.replicas
-        == context.create_whilrwind_resp.status.available_replicas
+        context.create_resp.status.replicas
+        == context.create_resp.status.available_replicas
     )
+
+@given(u"The deployment called {deployment_name} exists")
+def step_impl(context, deployment_name):
+    env = Environment(
+        loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
+    )
+    deployment_tmpl = env.get_template("deployment.j2")
+    deployment_spec_file = deployment_tmpl.render(deployment_name=deployment_name)
+    context.create_resp = deployment.create(
+        context.k8s_v1_apps_client, deployment_spec_file
+    )
+
+
+@then(u"Results for the deployment fire are returned")
+def step_impl(context):
+    assert context.get_resp, "Should've returned a valid response"
+
+
