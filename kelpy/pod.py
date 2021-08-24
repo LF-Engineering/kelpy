d4b2ad7733d8e534f0445eee0fb3405de15d49c4
Paul Myjavec <pauly@myjavec.com>
2021-08-24 14:54:08 +0900

    Fix some valid issues identified with pylint
    Signed-off-by: Paul Myjavec <pauly@myjavec.com>

--- kelpy/pod.py
+++ kelpy/pod.py
@@ -1,7 +1,7 @@
 from kubernetes.client.exceptions import ApiException
 
 
-def list(client, label_selector, namespace="default"):
+def list_all(client, label_selector, namespace="default"):
     try:
         response = client.list_namespaced_pod(
             namespace=namespace, label_selector=label_selector
