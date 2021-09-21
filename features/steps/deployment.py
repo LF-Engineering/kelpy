f48069beffa1adead798fc4fafdf370503d206b7
Ram Reddy Amanaganti <ramreddya@biarca.com>
2021-09-21 08:11:37 +0000

    Updating the statements in tests
    Signed-off-by: Ram Reddy Amanaganti <ramreddya@biarca.com>

--- features/steps/deployment.py
+++ features/steps/deployment.py
@@ -5,22 +5,22 @@ from kubernetes import config, client
 from jinja2 import Environment, FileSystemLoader
 
 
-@given(u"A deployment called {deployment_name} does not exist")
+@given(u"a deployment called {deployment_name} does not exist")
 def step_impl(context, deployment_name):
     deployment.delete(context.k8s_v1_apps_client, deployment_name)
 
 
-@when(u"The user attempts to retrieve the deployment {deployment_name}")
+@when(u"the user attempts to retrieve the deployment {deployment_name}")
 def step_impl(context, deployment_name):
     context.get_resp = deployment.get(context.k8s_v1_apps_client, deployment_name)
 
 
-@then(u"None is returned for the deployment")
+@then(u"None is returned instead of the deployment")
 def step_impl(context):
     assert context.get_resp is None, "Did not return None"
 
 
-@when(u"The deployment called {deployment_name} is created")
+@when(u"the deployment called {deployment_name} is created")
 def step_impl(context, deployment_name):
     env = Environment(
         loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
@@ -40,7 +40,7 @@ def step_impl(context):
     )
 
 
-@given(u"The deployment called {deployment_name} exists")
+@given(u"the deployment called {deployment_name} exists")
 def step_impl(context, deployment_name):
     env = Environment(
         loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
