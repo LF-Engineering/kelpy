64180882cf2df76f532e4cf1b4378f3eba11ab8d
Pablo Jaramillo <juan@pablox.io>
2022-01-03 15:52:44 -0600

    Add Job support

--- features/steps/cronjob.py
+++ features/steps/cronjob.py
@@ -6,6 +6,7 @@ from jinja2 import Environment, FileSystemLoader
 
 
 @when("the CronJob called {cronjob_name} is created")
+@given("the CronJob called {cronjob_name} exists")
 def step_impl(context, cronjob_name):
     cronjob_namespace = "default"
 
@@ -31,29 +32,13 @@ def step_impl(context, cronjob_name):
     cronjob.delete(context.k8s_v1_batch_client, cronjob_name)
 
 
-@given("the CronJob called {cronjob_name} exists")
-def step_impl(context, cronjob_name):
-    cronjob_namespace = "default"
-
-    env = Environment(
-        loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
-    )
-    cronjob_tmpl = env.get_template("cronjob.j2")
-    cronjob_spec_file = cronjob_tmpl.render(
-        cronjob_name=cronjob_name, cronjob_namespace=cronjob_namespace
-    )
-    context.create_resp = cronjob.create(
-        context.k8s_v1_batch_client, cronjob_spec_file, cronjob_namespace
-    )
-
-
 @then("None is returned instead of the CronJob")
 def step_impl(context):
     assert context.get_resp is None, "Did not return None"
 
 
-@then("Results for the CronJob hello are returned")
-def step_impl(context):
+@then("Results for the CronJob {cronjob_name} are returned")
+def step_impl(context, cronjob_name):
     assert context.get_resp, "Should've returned a valid response"
 
 
