feafef4e705c7a779ad0ea0e07b9f334366f08c5
KCS <bhanuk@biarca.com>
2022-01-25 15:52:02 +0530

    fixed issues with pre-commit-run-all
    Signed-off-by: KCS <bhanuk@biarca.com>

--- features/limitrange.feature
+++ features/limitrange.feature
@@ -10,9 +10,8 @@ Feature: Test limitrange
             Given a limitrange called test1 does not exist
               When the limitrange called test1 is created
                 Then a valid limitrange called test1 can be found
-      
+
        Scenario: Retrieve a limitrange that exists
             Given the limitrange called test2 exists
               When the user attempts to retrieve the limitrange test2
                 Then Results for the limitrange test2 are returned
-
--- features/steps/limitrange.py
+++ features/steps/limitrange.py
@@ -5,11 +5,10 @@ from kubernetes import config, client
 from jinja2 import Environment, FileSystemLoader
 
 
-
 @when("the limitrange called {limitrange_name} is created")
 @given("the limitrange called {limitrange_name} exists")
 def step_impl(context, limitrange_name):
-    limitrange_namespace="default"
+    limitrange_namespace = "default"
 
     env = Environment(
         loader=FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True
@@ -17,29 +16,32 @@ def step_impl(context, limitrange_name):
 
     limitrange_tmpl = env.get_template("limitrange.j2")
     limitrange_spec_file = limitrange_tmpl.render(
-            limitrange_name=limitrange_name,  limitrange_namespace=limitrange_namespace
-            )
+        limitrange_name=limitrange_name, limitrange_namespace=limitrange_namespace
+    )
     context.create_resp = limitrange.create(
-            context.k8s_v1_core_client, limitrange_spec_file, limitrange_namespace
-            )
+        context.k8s_v1_core_client, limitrange_spec_file, limitrange_namespace
+    )
+
 
 @when("the user attempts to retrieve the limitrange {limitrange_name}")
 def step_impl(context, limitrange_name):
-    context.get_resp = limitrange.get(
-            context.k8s_v1_core_client, limitrange_name
-            )
+    context.get_resp = limitrange.get(context.k8s_v1_core_client, limitrange_name)
+
 
 @given("a limitrange called {limitrange_name} does not exist")
 def step_impl(context, limitrange_name):
     limitrange.delete(context.k8s_v1_core_client, limitrange_name)
 
+
 @then("None is returned instead of the limitrange")
 def step_impl(context):
     assert context.get_resp is None, "Did not return None"
 
+
 @then("Results for the limitrange {limitrange_name} are returned")
 def step_impl(context, limitrange_name):
     assert context.get_resp, "Should've returned a valid response"
+
 
 @then("a valid limitrange called {limitrange_name} can be found")
 def step_impl(context, limitrange_name):
